0cc5f4f66da63fa9ab6388e48bff1b67f3117495
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-17 11:30:37 +0200

    Oprettelse

---
+++ src/Opgave_4/Opgave4.java
@@ -0,0 +1,17 @@
+package Opgave_4;
+
+public class Opgave4 {
+    public static void main(String[] args) {
+        // test case
+        // use three variables ""first"" && "Second" && "Third"
+        // give those variables a value
+        // test which has the highest values
+
+        int first = 55;
+        int second = 54;
+        int third = 53;
+
+
+
+    }
+}
---
+++ src/opgave_3/Opgave3.java
@@ -0,0 +1,22 @@
+package opgave_3;
+
+public class Opgave3 {
+
+    public static void main(String[] args) {
+        // test case
+        // use two variables ""first"" || "Second"
+        // give those variables a value
+        // test which has the highest values
+
+        int first = 55;
+        int second = 54;
+        // inputs = 50, 45
+        // expected result "First value is higher"
+
+        if (first>=second)
+            System.out.println("Pass");
+        else
+            System.out.println("fail");
+
+    }
+}
